f93744858272e50757d0dc15575aaa123cadf08f
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-09 22:41:18 -0700

    commit tree 1cecaf9f82f4746e7a334bef32e5a63fbe2e7702

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    }),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      renderUser(object);
+    })
+    .catch(function (error) {
+      renderError(error.message);
+      // renderUser(error.message);
+    });
+}
+
+const body = document.querySelector("body");
+
+function renderUser(user) {
+  const p = document.createElement("p");
+  p.innerHTML = user.id;
+  // p.id = user.id;
+  body.appendChild(p);
+}
+
+function renderError(message) {
+  const p = document.createElement("p");
+  p.innerHTML = message;
+  body.appendChild(p);
+}
